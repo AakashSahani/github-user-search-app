f3508f2c1faa55d0a9dcdc9b1690bfb19c619dd7
AakashSahani <sahani.aakash120@gmail.com>
2023-11-14 19:33:29 -0500

    Added search functionality

--- script/app.js
+++ script/app.js
@@ -1,124 +1,87 @@
 const userSearchForm = document.getElementById('username-search-form');
-const githubMainContainer = document.getElementById('github-main');
+const userName = document.getElementById('username');
+const githubMainContainer = document.getElementById('github-profile-container');
 const themeInput = document.getElementById('theme');
 const themeLabel = document.getElementById('theme-label');
 
-const gitUserData = {
-	login: 'manish',
-	id: 16161,
-	node_id: 'MDQ6VXNlcjE2MTYx',
-	avatar_url: 'https://avatars.githubusercontent.com/u/16161?v=4',
-	gravatar_id: '',
-	url: 'https://api.github.com/users/manish',
-	html_url: 'https://github.com/manish',
-	followers_url: 'https://api.github.com/users/manish/followers',
-	following_url: 'https://api.github.com/users/manish/following{/other_user}',
-	gists_url: 'https://api.github.com/users/manish/gists{/gist_id}',
-	starred_url: 'https://api.github.com/users/manish/starred{/owner}{/repo}',
-	subscriptions_url: 'https://api.github.com/users/manish/subscriptions',
-	organizations_url: 'https://api.github.com/users/manish/orgs',
-	repos_url: 'https://api.github.com/users/manish/repos',
-	events_url: 'https://api.github.com/users/manish/events{/privacy}',
-	received_events_url: 'https://api.github.com/users/manish/received_events',
-	type: 'User',
-	site_admin: false,
-	name: 'Manish Sinha',
-	company: 'Amazon',
-	blog: 'https://manishsinhav.com/',
-	location: 'San Franciso Bay Area, CA',
-	email: null,
-	hireable: null,
-	bio: 'Software Engineer @amzn ',
-	twitter_username: null,
-	public_repos: 14,
-	public_gists: 4,
-	followers: 38,
-	following: 14,
-	created_at: '2008-07-06T08:14:46Z',
-	updated_at: '2023-10-25T16:55:22Z',
-};
-
 userSearchForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-	console.log('Form was submitted.');
-	getUserInfo('aakash');
-	// githubMainContainer.insertAdjacentElement('beforeend', userProfileTemplate);
-	const userinfo = document.createElement('div');
-	userinfo.innerHTML = userProfileTemplate;
-	githubMainContainer.appendChild(userinfo);
+	let userInfo = getUserInfo(userName.value);
+	userInfo.then((gitUserData) => {
+		let userProfileTemplate = `
+		<div>
+			<div>
+				<!-- User profile -->
+				<div><img src="./assets/icon-search.svg" alt="User profile" /></div>
+				<!-- User profile information, handle, date -->
+				<div>
+					<div>
+						<p>${gitUserData.name}</p>
+						<p${gitUserData.location}</p>
+					</div>
+					<div>Joined ${gitUserData.created_at}</div>
+				</div>
+			</div>
+			<!-- User Bio -->
+			<div>
+				<p>
+					${gitUserData.bio}
+				</p>
+			</div>
+			<!-- User Repos, Followers, Followings -->
+			<div>
+				<div>
+					<div>Repos</div>
+					<div>${gitUserData.public_repos}</div>
+				</div>
+				<div>
+					<div>Followers</div>
+					<div>${gitUserData.followers}</div>
+				</div>
+				<div>
+					<div>Following</div>
+					<div>${gitUserData.following}</div>
+				</div>
+			</div>
+			<!-- User Location, Twitter, Blog, Work -->
+			<div>
+				<div>
+					<img src="./assets/icon-location.svg" alt="Location" />
+					<span>${gitUserData.location}</span>
+				</div>
+				<div>
+					<img src="./assets/icon-website.svg" alt="Website" />
+					<a
+						href="https://github.blog
+					"
+						>${gitUserData.blog}</a
+					>
+				</div>
+				<div>
+					<img src="./assets/icon-twitter.svg" alt="Twitter" />
+					<span>Not Available</span>
+				</div>
+				<div>
+					<img src="./assets/icon-company.svg" alt="Company" />
+					<span>@github</span>
+				</div>
+			</div>
+		</div>
+`;
+		githubMainContainer.innerHTML = userProfileTemplate;
+	});
 });
 
 // getUserInfo function
 // api endpoint
 // https://api.github.com/users/{username}
 // input - username - String
-// output - result - json data
+// output - gitUserData - a promise
 const getUserInfo = async (username) => {
-	const response = await fetch(`https://api.github.com/users/john`)
-		.then((res) => res.json())
-		.then((data) => console.log(data));
+	const response = await fetch(`https://api.github.com/users/${username}`);
+	gitUserData = await response.json();
+	return gitUserData;
 };
-
-const userProfileTemplate = `
-			<div>
-				<div>
-					<!-- User profile -->
-					<div><img src="./assets/icon-search.svg" alt="User profile" /></div>
-					<!-- User profile information, handle, date -->
-					<div>
-						<div>
-							<p>${gitUserData.name}</p>
-							<p${gitUserData.location}</p>
-						</div>
-						<div>Joined ${gitUserData.created_at}</div>
-					</div>
-				</div>
-				<!-- User Bio -->
-				<div>
-					<p>
-						${gitUserData.bio}
-					</p>
-				</div>
-				<!-- User Repos, Followers, Followings -->
-				<div>
-					<div>
-						<div>Repos</div>
-						<div>${gitUserData.public_repos}</div>
-					</div>
-					<div>
-						<div>Followers</div>
-						<div>${gitUserData.followers}</div>
-					</div>
-					<div>
-						<div>Following</div>
-						<div>${gitUserData.following}</div>
-					</div>
-				</div>
-				<!-- User Location, Twitter, Blog, Work -->
-				<div>
-					<div>
-						<img src="./assets/icon-location.svg" alt="Location" />
-						<span>${gitUserData.location}</span>
-					</div>
-					<div>
-						<img src="./assets/icon-website.svg" alt="Website" />
-						<a
-							href="https://github.blog
-            "
-							>${gitUserData.blog}</a
-						>
-					</div>
-					<div>
-						<img src="./assets/icon-twitter.svg" alt="Twitter" />
-						<span>Not Available</span>
-					</div>
-					<div>
-						<img src="./assets/icon-company.svg" alt="Company" />
-						<span>@github</span>
-					</div>
-				</div>
-			</div>
-`;
 
 // Theme Change Implementation
 const moonSVG = `<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z" fill="#697C9A" fill-rule="nonzero"/></svg>`;
